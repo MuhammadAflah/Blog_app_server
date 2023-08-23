ac9e8c740843dc51e5cbb59126bc9b5ac7859bfd
Muhammad Aflah <muhammadaflah786@gmail.com>
2023-08-23 22:54:33 +0530

    post changed

--- utils/fetchPopulatedData.js
+++ utils/fetchPopulatedData.js
@@ -1,4 +1,4 @@
-import Post from "../models/Post.js";
+import Post from "../models/UserPost.js";
 
 export const fetchFindByIdData =async (id,data)=>{
     const populatedPost=await Post.findById(id,data)
